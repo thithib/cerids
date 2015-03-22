8c9f286d4668e1cf3db4423ac8e1a8b972537656
vzh <vzh@minet.net>
2015-03-22 23:31:31 +0100

    Adding parsing files

---
+++ src/parser.h
@@ -0,0 +1,14 @@
+/**
+ * \file parser.h
+ * \brief Header file for parser module
+ * \version 0.1
+ */
+#ifndef _PARSER_H
+#define _PARSER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+
